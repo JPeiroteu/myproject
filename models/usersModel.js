5b64cb7c654a6c27b9ed61363308452200bac4e3
Jorge Guedes <jorgeguedes16@gmail.com>
2023-03-06 10:51:03 +0000

    conections updates

--- models/usersModel.js
+++ models/usersModel.js
@@ -29,3 +29,22 @@ const users = [
 ];
 
 module.exports = User;
+
+
+const express = require('express');
+          const router = express.Router();
+          const Card = require("../models/cardsModel");
+          
+          router.get('/', async function(req, res, next) {
+              try { 
+                  console.log("Get all cards");
+                  let result = await Card.getAll();
+                  res.status(result.status).send(result.result);
+              } catch(err) {
+                  console.log(err);
+                  res.status(500).send(err);
+              }
+            });
+          
+          module.exports = router;
+
